86f94794e344834a305641432fe2cd1b5925a977
Lê Khánh Trình <vulekhanhtrinh2003@gmail.com>
2025-05-22 21:48:55 +0700

    adding shadcn & clerk authentication

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-50 py-12">
+      <div className="container mx-auto text-center text-gray-600">
+        <p>© 2025 AutoMarket. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-50 py-4">
+      <div className="container mx-auto">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
