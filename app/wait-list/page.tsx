563a6df01c0fd9df8942eae88de9405ab375088e
Lê Khánh Trình <vulekhanhtrinh2003@gmail.com>
2025-05-27 16:56:47 +0700

    wait-list page embed with Create & setup embed link

---
+++ app/wait-list/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WaitList = () => {
+  return (
+    <div className="mt-20 fixed w-full">
+      <div className="w-full">
+        <iframe
+          style={{ width: "100%", height: "100vh" }}
+          src="https://vehiql-waitlist-306.created.app"
+          title="Waiting List Landing Page"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default WaitList;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://vehiql-waitlist-306.created.app",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
